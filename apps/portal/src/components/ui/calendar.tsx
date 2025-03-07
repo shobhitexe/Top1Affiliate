79422f20b64edf58a6e765f200d7137371c5a34d
shobhitexe <sguptamds94@gmail.com>
2025-03-07 13:43:20 +0530

    Fix

--- apps/portal/src/components/ui/calendar.tsx
+++ apps/portal/src/components/ui/calendar.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker } from "react-day-picker";
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
   className,
@@ -60,6 +60,19 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        // Chevron: (props) => {
+        //   if (props.orientation === "left") {
+        //     return (
+        //       <ChevronLeftIcon
+        //         className={cn("h-4 w-4", className)}
+        //         {...props}
+        //       />
+        //     );
+        //   }
+        //   return (
+        //     <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        //   );
+        // },
         IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
@@ -69,8 +82,8 @@ function Calendar({
       }}
       {...props}
     />
-  )
+  );
 }
-Calendar.displayName = "Calendar"
+Calendar.displayName = "Calendar";
 
-export { Calendar }
+export { Calendar };
